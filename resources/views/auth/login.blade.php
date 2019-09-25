9cfa82534998b0186a6992631d1fcc3694427f97
Dario Salles <dariosalles@gmail.com>
2019-09-25 13:10:28 -0300

    25/09/2019 - 13:10

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,13 +11,28 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div> --}}
+
+
+                        <div class="form-group row">
+                            <label for="registro" class="col-md-4 col-form-label text-md-right">{{ __('Registro') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="registro" type="text" class="form-control @error('registro') is-invalid @enderror" name="registro" value="{{ old('registro') }}" required autocomplete="registro" autofocus>
+
+                                @error('registro')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
